616f6572ea774286c4de9732cec1495361689b7a
eirik lysenstoen <elysenstoen@gmail.com>
2020-08-18 21:29:31 +0200

    still working on the controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -8,10 +8,24 @@ class RestaurantsController < ApplicationController
 
   end
 
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+
+    redirect_to restaurant_path(@restaurant)
+  end
 
   private
 
   def find_restaurant
     @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :rating, :category, :phone_number)
   end
 end
