34f3e77623ed653908c8735daec62bf011a60c58
eirik lysenstoen <elysenstoen@gmail.com>
2020-08-20 19:44:01 +0200

    all green except the phone number very mysterious

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,6 @@ class Restaurant < ApplicationRecord
   has_many :reviews, dependent: :destroy
   validates :category, inclusion: { in: ["chinese", "italian", "japanese", "french", "belgian"],
     message: "%{value} is not a valid category" }, allow_nil: false
-  validates :name, :address, presence: true
+  validates :name, :address, :phone_number, presence: true
 
 end
